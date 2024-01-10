ae2a5cd4eefb65fabd5ac40d9fb4c650626eeabc
karinkoike <kktech162@gmail.com>
2024-01-10 18:30:23 +0900

    パスワードの正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
    validates :nickname, presence: true
-   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/ }
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
    validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
