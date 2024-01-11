eda628ecaab733a7bc8bd04bc72b9d3f71b2c326
karinkoike <kktech162@gmail.com>
2024-01-11 13:57:17 +0900

    商品出品ページを表示する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,22 @@
 class ItemsController < ApplicationController
-  def index
-    
+  before_action :authenticate_user!, only: [:new, :create]
+  
+  def new
+    @item = Item.new
   end
 
-  def new
-    
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_state_id, :delivery_charge_burden_id, :prefecture_id, :delivery_date_id, :price)
   end
 end
