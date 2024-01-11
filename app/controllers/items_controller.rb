9ffce06d96fc0fb2081f2f8fce4ef8aadf8a1310
karinkoike <kktech162@gmail.com>
2024-01-11 19:05:07 +0900

    エラー表記させる実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,15 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def new
     @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
+    @item.user = current_user
     if @item.save
-      
+      redirect_to root_path
     else
       render :new
     end
