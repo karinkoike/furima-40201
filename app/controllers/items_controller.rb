e21eaeeff219a59f8c7d3997d39356636bad5057
karinkoike <kktech162@gmail.com>
2024-01-17 15:35:53 +0900

    微調整①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if !user_signed_in? || current_user.id != @item.user_id || @item.orders.present?
+    if !user_signed_in? || current_user.id != @item.user_id || @item.order.present?
       redirect_to root_path
     end
 
